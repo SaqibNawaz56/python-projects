e6e2d60dadb39cddd522e2b57d3eb5d29779384d
saqibnawaz56 <saqibn336@gmail.com>
2026-02-19 12:12:19 +0500

    fix : take input from cli

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ from printing_results import print_results
 from hourly_logs import count_logs_per_hour, find_max_hour
 from counting_log import  count_log_levels
 from reading_logs import read_logs_file
-
+import argparse
 
 def main():
-    log_file_path = input("Please Enter the Path of File: ")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file_path")
+    args = parser.parse_args()
+    log_file_path = args.file_path
     lines = read_logs_file(log_file_path)
-
     if not lines:
         return
-
     count = count_log_levels(lines)
     hour_count = count_logs_per_hour(lines)
     max_hour, max_logs = find_max_hour(hour_count)
